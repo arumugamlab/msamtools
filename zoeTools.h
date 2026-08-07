fd8a5157252647c46e1656068472cb3549134751
microbiomix <56069970+microbiomix@users.noreply.github.com>
2026-08-07 15:38:23 +0000

    Re-derive zoeTools from SNAP commit 4ad1e95 under MIT license

--- zoeTools.h
+++ zoeTools.h
@@ -1,7 +1,28 @@
 /******************************************************************************\
  zoeTools.h - part of the ZOE library for genomic analysis
  
- Copyright (C) 2002-2005 Ian Korf
+Copyright (C) Ian Korf 2002-2013.
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be included
+in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+The MIT License (MIT) - opensource.org/licenses/MIT
 
 \******************************************************************************/
 
@@ -41,7 +62,6 @@ typedef struct zoeTVec * zoeTVec;
 void    zoeDeleteTVec (zoeTVec);
 zoeTVec zoeNewTVec (void);
 void    zoePushTVec (zoeTVec, const char *);
-int zoeTcmp (const void * a, const void * b);
 
 struct zoeVec  {
 	void ** elem;
